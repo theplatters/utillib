71525e46d7192eb079ed417d26618274ad8f88d1
JanusUranus <jakob.fromherz@gmail.com>
2022-02-19 13:04:33 +0100

    core functionality implemented

--- ds/BinaryTree.h
+++ ds/BinaryTree.h
@@ -1,1 +1,64 @@
+#ifndef TREES_BINARYTREE_H
+#define TREES_BINARYTREE_H
 
+template <typename T>
+class BinaryTree
+{
+public:
+    struct Node
+    {
+        T data;
+        Node* left;
+        Node* right;
+    };
+
+private:
+    Node* root;
+
+    Node* copyTree(Node* node);
+    void emptyRec(Node* node);
+public:
+    BinaryTree();
+    BinaryTree(BinaryTree& left, T element, BinaryTree& right);
+    ~BinaryTree();
+    void empty();
+
+};
+
+
+template <typename T>
+BinaryTree<T>::BinaryTree(): root(nullptr){}
+
+template<typename T>
+BinaryTree<T>::BinaryTree(BinaryTree& left, T element, BinaryTree& right)
+{
+    root = new Node({.data = element,.left = copyTree(left.root),.right = copyTree(right.root)});
+}
+
+template <typename T>
+typename BinaryTree<T>::Node* BinaryTree<T>::copyTree(Node* node)
+{
+    if(node!=nullptr)
+        return new Node({.data = node->data,.left = copyTree(node->left),.right = copyTree(node->right)});
+}
+
+template<typename T>
+void BinaryTree<T>::empty()
+{
+    emptyRec(root);
+    root = nullptr;
+}
+
+template <typename T>
+void BinaryTree<T>::emptyRec(Node* node)
+{
+    if(node!=nullptr)
+    {
+        empytRec(node->left);
+        emptyRec(node->right);
+        delete node;
+    }
+}
+
+
+#endif
