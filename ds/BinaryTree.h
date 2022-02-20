8eb61c603a8e6055ca368990b1d27d39dcabae84
theplatters <fscharnreitner@gmail.com>
2022-02-20 20:21:43 +0100

    formatting

--- ds/BinaryTree.h
+++ ds/BinaryTree.h
@@ -3,59 +3,65 @@
 #ifndef BINARYTREE_H
 #define BINARYTREE_H
 
-template <typename T>
-class BinaryTree
-{
+template<typename T>
+class BinaryTree {
 private:
-    struct Node
-    {
+    struct Node {
         T data;
-        Node* left;
-        Node* right;
+        Node *left;
+        Node *right;
     };
 
-    Node* root;
+    Node *root;
 
-    Node* copyTree(Node* node);
-    void emptyRec(Node* node);
-    void printBinaryTreeRec(Node* node);
+    Node *copyTree(Node *node);
+
+    void emptyRec(Node *node);
+
+    void printBinaryTreeRec(Node *node);
+
 public:
     BinaryTree();
-    BinaryTree(BinaryTree& left, T element, BinaryTree& right);
+
+    BinaryTree(BinaryTree &left, T element, BinaryTree &right);
+
     ~BinaryTree();
+
     void empty();
+
     bool isEmpty();
-    void makeTree(BinaryTree& left, T element, BinaryTree& right);
+
+    void makeTree(BinaryTree &left, T element, BinaryTree &right);
+
     BinaryTree leftTree();
+
     BinaryTree rightTree();
+
     T key();
+
     void printBinaryTree();
 
 };
 
 
-template <typename T>
-BinaryTree<T>::BinaryTree(): root(nullptr){}
+template<typename T>
+BinaryTree<T>::BinaryTree(): root(nullptr) {}
 
 template<typename T>
-BinaryTree<T>::BinaryTree(BinaryTree& left, T element, BinaryTree& right)
-{
-    root = new Node({.data = element,.left = copyTree(left.root),.right = copyTree(right.root)});
+BinaryTree<T>::BinaryTree(BinaryTree &left, T element, BinaryTree &right) {
+    root = new Node({.data = element, .left = copyTree(left.root), .right = copyTree(right.root)});
 }
 
-template <typename T>
-typename BinaryTree<T>::Node* BinaryTree<T>::copyTree(Node* node)
-{
-    if(node==nullptr)
+template<typename T>
+typename BinaryTree<T>::Node *BinaryTree<T>::copyTree(Node *node) {
+    if (node == nullptr)
         return node;
-    return new Node({.data = node->data,.left = copyTree(node->left),.right = copyTree(node->right)});
+    return new Node({.data = node->data, .left = copyTree(node->left), .right = copyTree(node->right)});
 }
 
-template <typename T>
-void BinaryTree<T>::emptyRec(Node* node)
-{
-    if(node!=nullptr)
-    {
+template<typename T>
+void BinaryTree<T>::emptyRec(Node *node) {
+    if (node != nullptr) {
         emptyRec(node->left);
         emptyRec(node->right);
         delete node;
@@ -63,48 +69,41 @@ void BinaryTree<T>::emptyRec(Node* node)
 }
 
 template<typename T>
-void BinaryTree<T>::empty()
-{
+void BinaryTree<T>::empty() {
     emptyRec(root);
     root = nullptr;
 }
 
-template <typename T>
-bool BinaryTree<T>::isEmpty(){return root==nullptr;}
+template<typename T>
+bool BinaryTree<T>::isEmpty() { return root == nullptr; }
 
 template<typename T>
-void BinaryTree<T>::makeTree(BinaryTree& left, T element, BinaryTree& right)
-{
-    if(isEmpty())
-        root = new Node({.data = element,.left = copyTree(left.root), .right = copyTree(right.root)});
+void BinaryTree<T>::makeTree(BinaryTree &left, T element, BinaryTree &right) {
+    if (isEmpty())
+        root = new Node({.data = element, .left = copyTree(left.root), .right = copyTree(right.root)});
     else
         std::cout << "Tree non-empty, cannot make new tree" << std::endl;
 }
 
-template <typename T>
-BinaryTree<T>::~BinaryTree()
-{
+template<typename T>
+BinaryTree<T>::~BinaryTree() {
     empty();
     //root = nullptr;
 }
 
-template <typename T>
-BinaryTree<T> BinaryTree<T>::leftTree()
-{
-    return BinaryTree({.root = this->root == nullptr?nullptr:copyTree(this->root->left)});
+template<typename T>
+BinaryTree<T> BinaryTree<T>::leftTree() {
+    return BinaryTree({.root = this->root == nullptr ? nullptr : copyTree(this->root->left)});
 }
 
-template <typename T>
-BinaryTree<T> BinaryTree<T>::rightTree()
-{
-    return BinaryTree({.root = this->root == nullptr?nullptr:copyTree(this->root->right)});
+template<typename T>
+BinaryTree<T> BinaryTree<T>::rightTree() {
+    return BinaryTree({.root = this->root == nullptr ? nullptr : copyTree(this->root->right)});
 }
 
-template <typename T>
-T BinaryTree<T>::key()
-{
-    if(isEmpty())
-    {
+template<typename T>
+T BinaryTree<T>::key() {
+    if (isEmpty()) {
         T dummy;
         std::cout << "Tree is empty, cannot retrieve key" << std::endl;
         return dummy;
@@ -112,14 +111,16 @@ T BinaryTree<T>::key()
     return root->data;
 }
 
-template <typename T>
-void BinaryTree<T>::printBinaryTree(){std::cout <<"["; printBinaryTreeRec(root); std::cout <<"]" << std::endl;}
+template<typename T>
+void BinaryTree<T>::printBinaryTree() {
+    std::cout << "[";
+    printBinaryTreeRec(root);
+    std::cout << "]" << std::endl;
+}
 
-template <typename T>
-void BinaryTree<T>::printBinaryTreeRec(Node* node)
-{
-    if(node!= nullptr)
-    {
+template<typename T>
+void BinaryTree<T>::printBinaryTreeRec(Node *node) {
+    if (node != nullptr) {
         printBinaryTreeRec(node->left);
         std::cout << node->data << " ";
         printBinaryTreeRec(node->right);
--- main.cpp
+++ main.cpp
@@ -1,77 +1,8 @@
-#include <iostream>
-#include <vector>
-#include "sorting.h"
-#include "LinkedList.h"
-#include "Queue.h"
+
 #include "BinaryTree.h"
-#include <cmath>
 
-int *merge(int *arr, int *aux, int begin, int mid, int end) {
-    for (int i = begin; i <= end; i++)
-        aux[i] = arr[i];
-    int i = begin;
-    int j = mid;
-    while (i < mid && j <= end) {
-        if (aux[i] < aux[j]) {
-            arr[begin] = aux[i];
-            begin++;
-            i++;
-        } else {
-            arr[begin] = aux[j];
-            begin++;
-            j++;
-        }
-    }
-    while (i < mid)
-        arr[begin++] = aux[i++];
-    while (j <= end)
-        arr[begin++] = aux[j++];
-    return arr;
-}
-
-int *mergeSort(int *arr, int *aux, int begin, int end) {
-    if (begin >= end)
-        return arr;
-    int mid = floor((begin + end) / 2);
-    arr = mergeSort(arr, aux, begin, mid);
-    arr = mergeSort(arr, aux, mid + 1, end);
-    return merge(arr, aux, begin, mid + 1, end);
-}
-
-void testJakobMerge(){
-
-
-    srand(time(0));
-    int n = 10;
-    int *arr = new int[n];
-    int *aux = new int[n];
-    // int arr[n];
-    // int aux[n];
-
-
-    for (int i = 0; i < n; i++) {
-        arr[i] = rand() % (10 * n);
-        std::cout << arr[i] << std::endl;
-    }
-    std::cout << std::endl;
-    mergeSort(arr, aux, 0, n);
-
-    for (int i = 0; i < n; i++)
-        std::cout << arr[i] << std::endl;
-
-    delete[] arr;
-    delete[] aux;
-
-}
+#include <string>
 
 int main() {
 
-    LinkedList<int> t = {1,2,3,4,5};
-    LinkedList<int> t2 = {6,7,8,9,10};
-
-
-
-    t.printList();
-
-    return 0;
 }
